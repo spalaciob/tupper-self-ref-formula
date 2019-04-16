3e01f3519de890f0a4148e549a0a33b754e93e41
Sebastian <spalaciob@users.noreply.github.com>
2019-04-16 20:28:44 +0200

    Ammended some comments

--- tupper.py
+++ tupper.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 """
-DESCRIPTION: Implementation of Tupper's Self-Referencial Formula
+DESCRIPTION: Implementation of Tupper's Self-Referential Formula
 
 A bit more flexible that the ones found online so far, it allows plotting
 the result of applying the formula, given a value of k
@@ -78,7 +78,7 @@ if __name__ == "__main__":
         print('Using provided k...')
         k = opts.k
     h, w = H, W
-    # Original Tupper's self-referencing function
+    # k to plot Tupper's Self-Referential Formula
     # k = 960939379918958884971672962127852754715004339660129306651505519271702802395266424689642842174350718121267153782770623355993237280874144307891325963941337723487857735749823926629715517173716995165232890538221612403238855866184013235585136048828693337902491454229288667081096184496091705183454067827731551705405381627380967602565625016981482083418783163849115590225610003652351370343874461848378737238198224849863465033159410054974700593138339226497249461751545728366702369745461014655997933798537483143786841806593422227898388722980000748404719
 
     x = np.arange(w)[:, np.newaxis]
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     bin_map = tupper(x, y+k*h)
 
     plt.imshow(np.rot90(bin_map), plt.cm.Greys)
-    # plt.savefig('tupper_k{}')
+    # plt.savefig('tupper.png')
     try:
         input("Press key")
     except:
